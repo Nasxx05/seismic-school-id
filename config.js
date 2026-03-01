3f87c25b247d2cc6056126568408158cffbc7843
nas <101592913+Nasxx05@users.noreply.github.com>
2026-03-01 11:42:42 +0100

    Update config.js

--- config.js
+++ config.js
@@ -5,5 +5,5 @@
 // 2. Go to Project Settings → API
 // 3. Copy the "Project URL" and "anon public" key below
 
-const SUPABASE_URL = 'YOUR_SUPABASE_URL';
-const SUPABASE_ANON_KEY = 'YOUR_SUPABASE_ANON_KEY';
+const SUPABASE_URL = 'https://owzdqvwmycrjkuxazzch.supabase.co';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im93emRxdndteWNyamt1eGF6emNoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzIzNTc2MTksImV4cCI6MjA4NzkzMzYxOX0.n0ZVI2jEtmkZc6cFGuEfDx6z3ussj24sR45Zv4SLo2U';
